4dc3d14c7d3f6bacc3e346450aa32007542b22fa
marcianobeat <marcianobeat7@gmail.com>
2025-05-24 00:50:41 +0000

    teste1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,15 @@ int main(){
     int Nturistico;
     char cidade[33];
 
+    int populacao2;
+    float area2;
+    float pib2;
+    int Nturistico2;
+    char cidade2[33];
+
+
+    printf("")
+
     printf("Digite sua cidade 01: \n");
     scanf("%s", &cidade);
 
@@ -22,7 +31,28 @@ int main(){
     printf("O número de pontos turísticos é de: \n");
     scanf("%d", &Nturistico);
 
-     printf(" Cidade 01:%s \n População:%d \n Área:%f \n PIB:%f \n Número de pontos turísticos:%d \n", cidade, populacao, area, pib, Nturistico);
+
+
+
+     printf("Digite sua cidade 02: \n");
+    scanf("%s", &cidade2);
+
+    printf("O numero de população é de: \n");
+    scanf("%d", &populacao2);
+    
+    printf("Digite o tamanho da área: \n");
+    scanf("%f", &area2);
+
+    printf("O PIB é: \n");
+    scanf("%f", &pib2);
+
+    printf("O número de pontos turísticos é de: \n \n");
+    scanf("%d", &Nturistico2);
+
+
+    printf(" Cidade 01:%s \n População:%d \n Área:%f \n PIB:%f \n Número de pontos turísticos:%d \n \n", cidade, populacao, area, pib, Nturistico);
+
+     printf(" Cidade 02:%s \n População:%d \n Área:%f \n PIB:%f \n Número de pontos turísticos:%d \n", cidade2, populacao2, area2, pib2, Nturistico2);
     
      return 0;
 
